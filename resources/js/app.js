a7b1e33c44a645625adf87c51f20ce2ccbe60e21
JOANSAGA <joansaga@outlook.com>
2021-05-03 22:57:32 -0500

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,13 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import Vue from 'vue'
+
+//Main pages
+import App from './vue/app.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
---
+++ script.sql
@@ -0,0 +1,119 @@
+INSERT INTO `carreras` (
+        `id`,
+        `nombre_carrera`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'SISTEMAS',
+        '2021-05-03 22:05:55',
+        '2021-05-03 22:05:55'
+    );
+INSERT INTO `eventos` (`id`, `nombre`, `created_at`, `updated_at`)
+VALUES (
+        NULL,
+        'Conferencia: Nuevas tecnologías',
+        '2021-05-03 22:10:04',
+        '2021-05-03 22:10:04'
+    );
+INSERT INTO `configuraciones` (
+        `id`,
+        `dia`,
+        `numero_citas`,
+        `hora_inicio`,
+        `hora_fin`,
+        `id_evento`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'LUNES',
+        '20',
+        '8',
+        '18',
+        '1',
+        '2021-05-03 22:12:01',
+        '2021-05-03 22:12:01'
+    );
+INSERT INTO `configuraciones` (
+        `id`,
+        `dia`,
+        `numero_citas`,
+        `hora_inicio`,
+        `hora_fin`,
+        `id_evento`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'MARTES',
+        '20',
+        '8',
+        '18',
+        '1',
+        '2021-05-03 22:12:01',
+        '2021-05-03 22:12:01'
+    );
+INSERT INTO `configuraciones` (
+        `id`,
+        `dia`,
+        `numero_citas`,
+        `hora_inicio`,
+        `hora_fin`,
+        `id_evento`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'MIERCOLES',
+        '20',
+        '8',
+        '18',
+        '1',
+        '2021-05-03 22:12:01',
+        '2021-05-03 22:12:01'
+    );
+INSERT INTO `configuraciones` (
+        `id`,
+        `dia`,
+        `numero_citas`,
+        `hora_inicio`,
+        `hora_fin`,
+        `id_evento`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'JUEVES',
+        '20',
+        '8',
+        '18',
+        '1',
+        '2021-05-03 22:12:01',
+        '2021-05-03 22:12:01'
+    );
+INSERT INTO `configuraciones` (
+        `id`,
+        `dia`,
+        `numero_citas`,
+        `hora_inicio`,
+        `hora_fin`,
+        `id_evento`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'VIERNES',
+        '10',
+        '8',
+        '18',
+        '1',
+        '2021-05-03 22:12:01',
+        '2021-05-03 22:12:01'
+    );
